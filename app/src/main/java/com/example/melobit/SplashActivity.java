5d1b3d24d74c11604032b5eb1f4bbdd4f14d5c02
elahe <elahemahmoodi81@gmail.com>
2022-12-31 23:27:43 +0330

    Splash screen progress bar & timer added

--- app/src/main/java/com/example/melobit/SplashActivity.java
+++ app/src/main/java/com/example/melobit/SplashActivity.java
@@ -2,7 +2,11 @@ package com.example.melobit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        },3000);
+
+
     }
 }
